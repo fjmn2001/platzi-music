ef87c4442a466edc8633cc37a4d4b636dd8c3bb3
Francisco Marcano <fjmn2001@gmail.com>
2020-02-21 22:40:47 -0400

    adding pug

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
-<template>
-    <div id="app">
-        <img alt="logo" src="./assets/logo.png">
-        <h1>{{ msg }}</h1>
-    </div>
+<template lang="pug">
+    #app
+        img(src="https://static.platzi.com/media/user_upload/Page%201-d8d869f4-7130-4ae9-9907-2fa809bd136a.jpg")
 </template>
 
 <script>
@@ -18,4 +16,9 @@
 
 <style lang="scss">
     @import "./scss/main";
+
+    #app {
+        font-family: 'Roboto', serif;
+        font-weight: bold;
+    }
 </style>
